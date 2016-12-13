6c45eab000466a47576468f6e4235d63a0c7be54
Julio Sena <julio.657.js@gmail.com>
2016-12-13 15:55:32 -0500

    fix tests and update navbar wuth categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -26,7 +26,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   test 'should redirect create when admin not legged in' do
     assert_no_difference 'Category.count' do
-      post :create , category: {name: 'sportss'}
+      post :create , category: {name: 'sports'}
     end
     assert_redirected_to categories_path
   end
